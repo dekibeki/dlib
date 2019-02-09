99397f88aa47c3336b0409a63c812f894762723d
josipk <josipk@student.unimelb.edu.au>
2019-02-09 13:46:25 +1100

    changes/fixes to array_view. Removed dynamic_array, just use std::vector

--- dlib/arrays.hpp
+++ dlib/arrays.hpp
@@ -8,202 +8,131 @@
 
 namespace dlib {
   namespace arrays_impl {
-    template<typename T_, typename Holding_pointer_>
-    class Basic_array_view {
-    public:
-      using Holding_pointer = Holding_pointer_;
-      using Size_arg = size_t;
-      using Holding_arg = T_;
-
-      using value_type = Holding_arg;
-      using size_type = Size_arg;
-      using reference = Holding_arg & ;
-      using const_reference = Holding_arg const&;
-      using pointer = Holding_arg * ;
-      using const_pointer = const Holding_arg*;
-      using iterator = pointer;
-      using const_iterator = const_pointer;
-      using reverse_iterator = std::reverse_iterator<iterator>;
-      using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-
-      constexpr Basic_array_view() noexcept :
-        ptr_{ nullptr },
-        size_{ 0 } {
-
-      }
-
-      constexpr Basic_array_view(nullptr_t) noexcept :
-        ptr_{ nullptr },
-        size_{ 0 } {
-
-      }
-
-      constexpr Basic_array_view(Holding_pointer ptr, size_type size) noexcept :
-        ptr_{ std::move(ptr) },
-        size_{ size } {
-
-      }
-
-      constexpr pointer data() const noexcept {
-        return &ptr_[0];
-      }
-
-      constexpr bool empty() const noexcept {
-        return size() == 0;
-      }
-
-      constexpr Size_arg size() const noexcept {
-        return size_;
-      }
-
-      constexpr Size_arg max_size() const noexcept {
-        return size();
-      }
-
-      constexpr void fill(const_reference value) noexcept {
-        for (Size_arg i = 0; i < size_; ++i) {
-          data()[i] = value;
-        }
-      }
-
-      constexpr reference operator[](size_type i) noexcept {
-        return data()[i];
-      }
-      constexpr const_reference operator[](size_type i) const noexcept {
-        return data()[i];
-      }
-
-      constexpr reference at(size_type i) noexcept {
-        if (i < 0 || i >= size_) {
-          //ERROR
-        }
-        return data()[i];
-      }
-      constexpr const_reference at(size_type i) const noexcept {
-        if (i < 0 || i >= size_) {
-          //ERROR
-        }
-        return data()[i];
-      }
-
-      constexpr reference front() noexcept {
-        return data()[0];
-      }
-
-      constexpr const_reference front() const noexcept {
-        return data()[0];
-      }
-
-      constexpr reference back() noexcept {
-        return data()[size() - 1];
-      }
-
-      constexpr const_reference back() const noexcept {
-        return data()[size() - 1];
-      }
-
-      constexpr iterator begin() noexcept {
-        return data();
-      }
-      constexpr const_iterator begin() const noexcept {
-        return data();
-      }
-      constexpr const_iterator cbegin() const noexcept {
-        return data();
-      }
-      constexpr iterator end() noexcept {
-        return data() + size_;
-      }
-      constexpr const_iterator end() const noexcept {
-        return data() + size_;
-      }
-      constexpr const_iterator cend() const noexcept {
-        return data() + size_;
-      }
-      protected:
-
-    private:
-      Holding_pointer ptr_;
-      size_type size_;
-    };
+    template<typename T>
+    constexpr bool is_valid_array_view_container = true;
   }
 
-  template<typename T>
-  class Dynamic_array :
-    public arrays_impl::Basic_array_view<T, std::unique_ptr<T[]>> {
+  template<typename T_>
+  class Array_view {
   public:
-    using value_type = typename Basic_array_view::value_type;
-    using size_type = typename Basic_array_view::size_type;
+    using value_type = T_;
+    using size_type = std::size_t;
+    using reference = value_type & ;
+    using const_reference = value_type const&;
+    using pointer = value_type * ;
+    using const_pointer = const value_type*;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    constexpr Dynamic_array(nullptr_t) noexcept :
-      Basic_array_view{ nullptr } {
-
-    }
-
-    constexpr Dynamic_array(size_type n) noexcept :
-      Basic_array_view{ std::make_unique<T[]>(n), n } {
-
-    }
-  };
-
-  template<typename T>
-  class Array_view :
-    public arrays_impl::Basic_array_view<T, T*> {
-  public:
-    using Basic_array_view::Basic_array_view;
-    using value_type = typename Basic_array_view::value_type;
-    using size_type = typename Basic_array_view::size_type;
+    constexpr  Array_view() noexcept :
+      ptr_{ nullptr },
+     size_{ 0 } {
     
-    static constexpr bool t_is_const = std::is_const_v<T>;
-    using Unconst_t = std::remove_const_t<T>;
+    }
 
-    constexpr Array_view(value_type& single) noexcept :
-      Basic_array_view{ &single,1 } {
+    constexpr Array_view(nullptr_t) noexcept :
+      ptr_{ nullptr },
+      size_{ 0 } {
 
     }
 
-    template<size_t n, typename = std::enable_if_t<t_is_const>>
-    constexpr Array_view(Unconst_t(&data)[n]) noexcept :
-      Basic_array_view{ +data, n } {
+    constexpr Array_view(pointer ptr, size_type size) noexcept :
+      ptr_{ std::move(ptr) },
+      size_{ size } {
+
     }
+
+    template<typename Container, typename = decltype(std::declval<Container>().data()), typename = decltype(std::declval<Container>().size())>
+    constexpr Array_view(Container&& container) noexcept :
+      Array_view{ container.data(), container.size() } {
+
+    }
+
     template<size_t n>
-    constexpr Array_view(value_type(&data)[n]) noexcept :
-      Basic_array_view{ +data, n } {
+    constexpr Array_view(value_type(&p)[n]) noexcept :
+      Array_view{ +p, n } {
 
     }
 
-    template<size_t n>
-    constexpr Array_view(std::array<value_type, n>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
+    constexpr pointer data() const noexcept {
+      return &ptr_[0];
     }
 
-    template<size_t n, typename = std::enable_if_t<t_is_const>>
-    constexpr Array_view(std::array<Unconst_t, n>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
+    constexpr bool empty() const noexcept {
+      return size() == 0;
     }
 
-    constexpr Array_view(std::vector<value_type>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
-
+    constexpr size_type size() const noexcept {
+      return size_;
     }
 
-    template<typename = std::enable_if_t<t_is_const>>
-    constexpr Array_view(std::vector<Unconst_t>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
-
+    constexpr size_type max_size() const noexcept {
+      return size();
     }
 
-    constexpr Array_view(Dynamic_array<value_type>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
-
+    constexpr void fill(const_reference value) noexcept {
+      for (auto& ref : *this) {
+        ref = value;
+      }
     }
 
-    template<typename = std::enable_if_t<t_is_const>>
-    constexpr Array_view(Dynamic_array<Unconst_t>& data) noexcept :
-      Basic_array_view{ data.data(), data.size() } {
-
+    constexpr reference operator[](size_type i) noexcept {
+      return data()[i];
     }
-  
+    constexpr const_reference operator[](size_type i) const noexcept {
+      return data()[i];
+    }
+
+    constexpr reference at(size_type i) noexcept {
+      if (i < 0 || i >= size_) {
+        //ERROR
+      }
+      return data()[i];
+    }
+    constexpr const_reference at(size_type i) const noexcept {
+      if (i < 0 || i >= size_) {
+        //ERROR
+      }
+      return data()[i];
+    }
+
+    constexpr reference front() noexcept {
+      return data()[0];
+    }
+
+    constexpr const_reference front() const noexcept {
+      return data()[0];
+    }
+
+    constexpr reference back() noexcept {
+      return data()[size() - 1];
+    }
+
+    constexpr const_reference back() const noexcept {
+      return data()[size() - 1];
+    }
+
+    constexpr iterator begin() noexcept {
+      return data();
+    }
+    constexpr const_iterator begin() const noexcept {
+      return data();
+    }
+    constexpr const_iterator cbegin() const noexcept {
+      return data();
+    }
+    constexpr iterator end() noexcept {
+      return data() + size_;
+    }
+    constexpr const_iterator end() const noexcept {
+      return data() + size_;
+    }
+    constexpr const_iterator cend() const noexcept {
+      return data() + size_;
+    }
+
     constexpr Array_view subarray_unsafe(size_type offset, size_type len) const noexcept {
       return Array_view{ data() + offset, len };
     }
@@ -222,7 +151,7 @@ namespace dlib {
     constexpr Array_view subarray_unsafe(size_type offset) const noexcept {
       return subarray_unsafe(offset, size() - offset);
     }
-    
+
     constexpr Array_view subarray_safe(size_type offset) const noexcept {
       return subarray_safe(offset, size() - offset);
     }
@@ -230,9 +159,15 @@ namespace dlib {
     constexpr Array_view subarray(size_type offset) const noexcept {
       return subarray(offset, size() - offset);
     }
-    
-    constexpr operator Array_view<const T>() const noexcept {
-      return Array_view<const T>(data(), size());
+
+    constexpr operator Array_view<const T_>() const noexcept {
+      return Array_view<const T_>(data(), size());
     }
+  private:
+    pointer ptr_;
+    size_type size_;
   };
+
+  template<typename Container>
+  Array_view(Container&&) noexcept -> Array_view<typename std::decay_t<Container>::value_type>;
 }
--- test/test_arrays.cpp
+++ test/test_arrays.cpp
@@ -3,19 +3,9 @@
 
 #include <dlib/arrays.hpp>
 
-
-
-BOOST_AUTO_TEST_CASE(dynamic_array) {
-
-  dlib::Dynamic_array<size_t> array{ 5 };
-  for (size_t i = 0; i < array.size(); ++i) {
-    array[i] = i;
-  }
-}
-
 BOOST_AUTO_TEST_CASE(array_view_raw_construction) {
   size_t test[5];
-  dlib::Array_view<size_t> view{test};
+  dlib::Array_view view{test};
   for (size_t i = 0; i < view.size(); ++i) {
     view[i] = i;
   }
@@ -23,7 +13,7 @@ BOOST_AUTO_TEST_CASE(array_view_raw_construction) {
 
 BOOST_AUTO_TEST_CASE(array_view_std_array_construction) {
   std::array<size_t, 5> test;
-  dlib::Array_view<size_t> view{ test };
+  dlib::Array_view view{ test };
   for (size_t i = 0; i < view.size(); ++i) {
     view[i] = i;
   }
@@ -32,17 +22,14 @@ BOOST_AUTO_TEST_CASE(array_view_std_array_construction) {
 BOOST_AUTO_TEST_CASE(array_view_std_vector_construction) {
   std::vector<size_t> test;
   test.resize(5);
-  dlib::Array_view<size_t> view{ test };
+  dlib::Array_view view{ test };
   for (size_t i = 0; i < view.size(); ++i) {
     view[i] = i;
   }
 }
 
-BOOST_AUTO_TEST_CASE(array_view_dynamic_array_construction) {
-  dlib::Dynamic_array<size_t> test{ 5 };
-  dlib::Array_view<size_t> view{ test };
-  for (size_t i = 0; i < view.size(); ++i) {
-    view[i] = i;
-  }
-}
+BOOST_AUTO_TEST_CASE(array_vew_const_value_type) {
+  std::array<const int, 5> test{ 0 };
 
+  dlib::Array_view view{ test };
+}
