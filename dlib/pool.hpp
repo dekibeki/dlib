94ce854376e25f19097002c564a59ba1259cf91c
josipk <josipk@student.unimelb.edu.au>
2019-02-13 16:05:31 +1100

    fix warnings from moving to clang

--- dlib/pool.hpp
+++ dlib/pool.hpp
@@ -108,8 +108,9 @@ namespace dlib {
     class Pool final :
       public Pool_base<Type_, Concurrency_, Pointer_> {
     public:
-      using Pooled = Pooled<Pool>;
-      using Pointer_to = Get_pointer_to<Pointer_arg, Pool>;
+      using Base = Pool_base<Type_, Concurrency_, Pointer_>
+      using Pooled = ::dlib::Pooled<Pool>;
+      using Pointer_to = Get_pointer_to<typename Base::Pointer_arg, Pool>;
       using Holder = Get_holder<Pointer_arg, Pool>;
 
       template<typename Constructor>
